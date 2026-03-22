1df9d9a50104198cae50083afbbca2046073fac3
Kyekyeku-Tech <safooppong121@gmail.com>
2026-03-22 18:09:01 +0000

    new update

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+    apiKey: "AIzaSyCFW4WMIvJjbFiyeym7I6OCk_FPAovAMIE",
+  authDomain: "payg-6219f.firebaseapp.com",
+  projectId: "payg-6219f",
+  storageBucket: "payg-6219f.firebasestorage.app",
+  messagingSenderId: "340280657976",
+  appId: "1:340280657976:web:b4e80a90254bb616aefecd",
+  measurementId: "G-YQ9CL0WR8G",
 };
 
 const requiredEnvValues = [
